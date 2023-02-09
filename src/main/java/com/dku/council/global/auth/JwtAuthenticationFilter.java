f552e1c48de153356fcc64ac8592891007378527
YOON <seoung59@gmail.com>
2023-02-09 19:20:50 +0900

    Feat : Security Filter Exception Handling

--- src/main/java/com/dku/council/global/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/dku/council/global/auth/JwtAuthenticationFilter.java
@@ -3,7 +3,11 @@ package com.dku.council.global.auth;
 import com.dku.council.global.auth.jwt.AuthenticationToken;
 import com.dku.council.global.auth.jwt.JwtAuthentication;
 import com.dku.council.global.auth.jwt.JwtAuthenticationTokenProvider;
+import com.dku.council.global.exception.CustomException;
+import com.dku.council.global.exception.ExceptionDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,13 +26,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        AuthenticationToken tokenFromHeader = jwtAuthenticationTokenProvider.getTokenFromHeader(request);
-        //인증 시작
-        if(tokenFromHeader.getAccessToken() != null){
-            //Authentication 등록
-            JwtAuthentication authentication = jwtAuthenticationTokenProvider.getAuthentication(tokenFromHeader.getAccessToken());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        try{
+            AuthenticationToken tokenFromHeader = jwtAuthenticationTokenProvider.getTokenFromHeader(request);
+            //인증 시작
+            if(tokenFromHeader.getAccessToken() != null){
+                //Authentication 등록
+                JwtAuthentication authentication = jwtAuthenticationTokenProvider.getAuthentication(tokenFromHeader.getAccessToken());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        }catch (CustomException e){
+            response.setStatus(e.getErrorCode().getStatus().value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+
+            ExceptionDto exceptionDto = new ExceptionDto(e);
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            String exceptionMessage = objectMapper.writeValueAsString(exceptionDto);
+
+            response.getWriter().write(exceptionMessage);
         }
-        filterChain.doFilter(request, response);
+
     }
 }
