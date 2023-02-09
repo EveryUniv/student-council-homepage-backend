3fc2b659717cb39ed10411ffade34b6019e64d85
won983212 <won983212@naver.com>
2023-02-09 21:25:04 +0900

    fix: Disabled Annotation 추가

--- src/test/java/com/dku/council/global/auth/jwt/JwtAuthenticationTokenProviderTest.java
+++ src/test/java/com/dku/council/global/auth/jwt/JwtAuthenticationTokenProviderTest.java
@@ -2,11 +2,13 @@ package com.dku.council.global.auth.jwt;
 
 import com.dku.council.domain.UserRole;
 import com.dku.council.domain.user.User;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class JwtAuthenticationTokenProviderTest {
 
     @Test
+    @Disabled
     void test1() {
         JwtAuthenticationTokenProvider jwtAuthenticationTokenProvider = new JwtAuthenticationTokenProvider(3L, 10L, "hello");
         AuthenticationToken issue = jwtAuthenticationTokenProvider.issue(new User(123L, UserRole.ADMIN));
