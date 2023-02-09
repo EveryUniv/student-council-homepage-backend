32789ff4e1f8dced1156467c03d2c89516586e2f
YOON <seoung59@gmail.com>
2023-02-09 19:06:58 +0900

    Feat: PasswordEncoder 추가

---
+++ src/main/java/com/dku/council/global/exception/ErrorCode.java
@@ -0,0 +1,11 @@
+package com.dku.council.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@AllArgsConstructor
+public class ErrorCode {
+
+}
