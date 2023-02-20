2d6a3c4acc26aacaa759bfc01185f249a0955371
won983212 <won983212@naver.com>
2023-02-21 00:41:32 +0900

    test: DkuAuthenticationService 테스트 추가

--- src/test/java/com/dku/council/infra/dku/service/DkuAuthenticationServiceTest.java
+++ src/test/java/com/dku/council/infra/dku/service/DkuAuthenticationServiceTest.java
@@ -1,13 +1,19 @@
 package com.dku.council.infra.dku.service;
 
-import com.dku.council.util.WebClientUtil;
+import com.dku.council.infra.dku.exception.DkuFailedLoginException;
+import com.dku.council.infra.dku.model.DkuAuth;
+import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DkuAuthenticationServiceTest {
 
@@ -29,5 +35,56 @@ class DkuAuthenticationServiceTest {
     @AfterAll
     static void afterAll() throws IOException {
         mockServer.shutdown();
+    }
+
+    @Test
+    @DisplayName("정상적인 흐름일 때 login성공?")
+    public void login() {
+        // given
+        mockServer.enqueue(new MockResponse()
+                .setResponseCode(302)
+                .addHeader("Location", "http://localhost:" + mockServer.getPort())
+                .addHeader("Set-Cookie", "cookie1=value1")
+                .addHeader("Set-Cookie", "cookie2=value2"));
+        mockServer.enqueue(new MockResponse()
+                .setResponseCode(302)
+                .addHeader("Set-Cookie", "cookie3=value3")
+                .addHeader("Set-Cookie", "cookie4=value4"));
+
+        // when
+        DkuAuth auth = service.login("32111111", "pwd");
+        MultiValueMap<String, String> actualCookies = new LinkedMultiValueMap<>();
+        auth.authCookies().accept(actualCookies);
+
+        // then
+        Assertions.assertThat(actualCookies)
+                .containsEntry("cookie1", List.of("value1"))
+                .containsEntry("cookie2", List.of("value2"))
+                .containsEntry("cookie3", List.of("value3"))
+                .containsEntry("cookie4", List.of("value4"));
+    }
+
+    @Test
+    @DisplayName("실패 - 302가 아닌 응답")
+    public void failedByNot302StatusCode() {
+        // given
+        mockServer.enqueue(new MockResponse());
+
+        // when & then
+        assertThrows(DkuFailedLoginException.class, () ->
+                service.login("32111111", "pwd"));
+    }
+
+    @Test
+    @DisplayName("실패 - sso로그인에서 302가 아닌 응답")
+    public void failedSSOByNot302StatusCode() {
+        // given
+        mockServer.enqueue(new MockResponse().setResponseCode(302)
+                .addHeader("Location", "http://localhost:" + mockServer.getPort()));
+        mockServer.enqueue(new MockResponse());
+
+        // when & then
+        assertThrows(DkuFailedLoginException.class, () ->
+                service.login("32111111", "pwd"));
     }
 }
