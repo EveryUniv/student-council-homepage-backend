9caaa71bb2acfade6fadfa91f7c71ea9b88d3c1b
won983212 <won983212@naver.com>
2023-02-25 02:08:50 +0900

    fix: redis password 추가

--- src/main/java/com/dku/council/global/config/RedisConfig.java
+++ src/main/java/com/dku/council/global/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -19,9 +20,17 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private final int port;
 
+    @Value("${spring.redis.password}")
+    private final String password;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
+        if (!password.isEmpty()) {
+            configuration.setPassword(RedisPassword.of(password));
+        }
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(configuration);
         lettuceConnectionFactory.afterPropertiesSet();
         return lettuceConnectionFactory;
     }
