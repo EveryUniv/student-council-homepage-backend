0e86873ea9b48d96fff54e636999a85ba7b8d8f5
YOON <seoung59@gmail.com>
2023-03-10 19:10:38 +0900

    Fix : import

--- src/test/java/com/dku/council/domain/page/service/PageServiceTest.java
+++ src/test/java/com/dku/council/domain/page/service/PageServiceTest.java
@@ -22,7 +22,6 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
