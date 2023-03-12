f8a006923617115fd644d044c8332864e9d4601d
won983212 <won983212@naver.com>
2023-03-12 15:17:49 +0900

    feat: 닉네임 중복 허용 안함

--- src/main/java/com/dku/council/domain/user/controller/UserController.java
+++ src/main/java/com/dku/council/domain/user/controller/UserController.java
@@ -13,7 +13,6 @@ import com.dku.council.global.auth.jwt.AppAuthentication;
 import com.dku.council.global.auth.role.UserOnly;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -52,6 +51,18 @@ public class UserController {
     public void signup(@Valid @RequestBody RequestSignupDto dto,
                        @PathVariable("signup-token") String signupToken) {
         signupService.signup(dto, signupToken);
+    }
+
+    /**
+     * 닉네임이 이미 존재하는지 검증.
+     * 닉네임이 이미 존재하는지 검증합니다. 만약 존재하지 않으면 OK를 반환하고,
+     * 이미 사용중인 경우에는 BAD_REQUEST 오류가 발생합니다.
+     *
+     * @param nickname 닉네임
+     */
+    @GetMapping("/valid")
+    public void validNickname(@RequestParam String nickname) {
+        signupService.checkAlreadyNickname(nickname);
     }
 
     /**
