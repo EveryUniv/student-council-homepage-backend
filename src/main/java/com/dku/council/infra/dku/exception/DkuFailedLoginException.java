1ebe671b82e46bf2b2ce16c56fa5bd214493c1ed
won983212 <won983212@naver.com>
2023-03-20 00:23:29 +0900

    refactor: custom message error 개선

--- src/main/java/com/dku/council/infra/dku/exception/DkuFailedLoginException.java
+++ src/main/java/com/dku/council/infra/dku/exception/DkuFailedLoginException.java
@@ -1,15 +1,9 @@
 package com.dku.council.infra.dku.exception;
 
 import com.dku.council.global.error.exception.LocalizedMessageException;
-import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 
-import java.util.List;
-import java.util.Locale;
-
 public class DkuFailedLoginException extends LocalizedMessageException {
-
-    private String customMessage = null;
 
     public DkuFailedLoginException() {
         super(HttpStatus.BAD_REQUEST, "failed.dku-login");
@@ -20,23 +14,7 @@ public class DkuFailedLoginException extends LocalizedMessageException {
     }
 
     public DkuFailedLoginException(String customMessage) {
-        super(HttpStatus.BAD_REQUEST, "failed.dku-login");
-        this.customMessage = customMessage;
-    }
-
-    @Override
-    public List<Object> getMessages(MessageSource messageSource, Locale locale) {
-        if (customMessage != null) {
-            return List.of(customMessage);
-        }
-        return super.getMessages(messageSource, locale);
-    }
-
-    @Override
-    public String getMessage() {
-        if (customMessage != null) {
-            return customMessage;
-        }
-        return super.getMessage();
+        super(HttpStatus.BAD_REQUEST, customMessage);
+        setCustomMessage(customMessage);
     }
 }
