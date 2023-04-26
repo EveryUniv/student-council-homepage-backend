773ced96cca797afa1bbf5d08e1329707bd1dde7
won983212 <won983212@naver.com>
2023-04-26 19:33:35 +0900

    test: 티켓 검증 서비스 테스트

---
+++ src/test/java/com/dku/council/domain/ticket/controller/TicketVerifyControllerTest.java
@@ -0,0 +1,26 @@
+package com.dku.council.domain.ticket.controller;
+
+import org.junit.jupiter.api.Test;
+
+class TicketVerifyControllerTest {
+
+    @Test
+    void myTicket() {
+    }
+
+    @Test
+    void getTicketInfo() {
+    }
+
+    @Test
+    void resendSms() {
+    }
+
+    @Test
+    void setToIssued() {
+    }
+
+    @Test
+    void setToUnissued() {
+    }
+}
