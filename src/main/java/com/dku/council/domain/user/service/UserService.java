074035f2eec96f15f7776117a95b9869c474135a
gutanbug53 <gutanbug53@gmail.com>
2023-05-04 14:04:28 +0900

    Fix: 유저 정보 중 개수 카운팅 수정

--- src/main/java/com/dku/council/domain/user/service/UserService.java
+++ src/main/java/com/dku/council/domain/user/service/UserService.java
@@ -23,7 +23,6 @@ import com.dku.council.global.auth.jwt.JwtProvider;
 import com.dku.council.global.error.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
