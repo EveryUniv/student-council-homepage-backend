7db3b758714dac38929572c18d46fa887dcdf95a
YOON <seoung59@gmail.com>
2023-07-03 23:49:49 +0900

    feat: AdminHomeBus create, update, delete

--- src/main/java/com/dku/council/domain/homebus/repository/HomeBusTicketRepository.java
+++ src/main/java/com/dku/council/domain/homebus/repository/HomeBusTicketRepository.java
@@ -2,6 +2,10 @@ package com.dku.council.domain.homebus.repository;
 
 import com.dku.council.domain.homebus.model.entity.HomeBusTicket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface HomeBusTicketRepository extends JpaRepository<HomeBusTicket, Long> {
+    Optional<HomeBusTicket> findByBusId(Long busId);
 }
